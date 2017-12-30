42dbffd3872862d428791452ea764edc049a8218
Unknown <justinasin@gmail.com>
2017-12-30 17:59:38 +0200

    New registration

---
+++ src/java/beans/RegistrationBean.java
@@ -0,0 +1,108 @@
+package beans;
+
+import javax.inject.Named;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@Named(value = "registrationBean")
+@ManagedBean
+@ViewScoped
+public class RegistrationBean implements Serializable {
+    
+    private String username;
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String address1;
+    private String address2;
+    private int phone1;
+    private int phone2;
+    private String gender;
+    private String email;
+            
+
+    public RegistrationBean() {
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+    
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+    
+    public int getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(int phone1) {
+        this.phone1 = phone1;
+    }
+    
+    public int getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(int phone2) {
+        this.phone2 = phone2;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
